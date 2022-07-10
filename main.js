58c9498da382c8d4a26991af573e3996da9fdf18
script <slmsttrv1@gmail.com>
2022-07-10 20:21:45 +0500

    flipper v0.2 done

--- main.js
+++ main.js
@@ -8,14 +8,15 @@ const colors = [
     "gray",
     "white",
     "green",
-    "purple"
+    "purple",
+    "brown"
 ];
 
 const colorName = document.getElementById('colorName');
 const btn = document.getElementById('btn');
 const bgc = document.querySelector('div.container__main');
 const para = document.getElementById('main__p');
-
+const colorTab = document.getElementById('color');
 
 
 function color(){
@@ -23,36 +24,52 @@ function color(){
     return a;
 }
 
-bgc.style.backgroundColor = color();
-
-btn.addEventListener('click', () =>{
+function randomColor(){
     let a = color();
     if(a == "red"){
         bgc.style.backgroundColor = "red";
+        colorName.innerHTML = "red";
     }else if(a == "blue"){
         bgc.style.backgroundColor = "blue";
+        colorName.innerHTML = "blue";
     }else if(a == "yellow"){
         bgc.style.backgroundColor = "yellow";
         para.style.color = "black";
+        colorName.innerHTML = "yellow";
     }else if(a == "black"){
         bgc.style.backgroundColor = "black";
         para.style.color = "white";
+        colorName.innerHTML = "black";
     }else if(a == "orange"){
         bgc.style.backgroundColor = "orange";
+        colorName.innerHTML = "orange";
     }else if(a == "pink"){
         bgc.style.backgroundColor = "pink";
+        colorName.innerHTML = "pink";
     }else if(a == "gray"){
         bgc.style.backgroundColor = "gray";
+        colorName.innerHTML = "gray";
     }else if(a == "white"){
         bgc.style.backgroundColor = "white";
         para.style.color = "black";
+        colorName.innerHTML = "white";
     }else if(a == "green"){
         bgc.style.backgroundColor = "green";
+        colorName.innerHTML = "green";
     }else if(a == "purple"){
         bgc.style.backgroundColor = "purple";
         para.style.color = "white";
+        colorName.innerHTML = "purple";
+    }else if(a == "brown"){
+        bgc.style.backgroundColor = "brown";
+        colorName.innerHTML = "brown";
     }
+}
+
+bgc.style.color = randomColor();
+
+btn.addEventListener('click', () =>{
+    randomColor();
 })
 
-// bgc.style.backgroundColor = "blue";
 
